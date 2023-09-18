f9b17761de3ba2f69ba521624938e2503c427d77
Michael Gallimore <mike@easy-expense.com>
2023-09-18 15:26:17 -0700

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='cnn for recognizing olive sided flycatcher calls in audio recordings.',
+    author='Michael Gallimore',
+    license='',
+)
