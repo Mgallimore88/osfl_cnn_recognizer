140a148280cea4d4434e5acd6ea18f5a6099b52c
Michael Gallimore <mike@easy-expense.com>
2023-09-21 12:32:25 -0700

    cleans some datatypes in csv

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,7 +2,7 @@
 import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+# from dotenv import find_dotenv, load_dotenv
 
 
 @click.command()
@@ -79,6 +79,7 @@ def main(input_filepath, output_filepath):
         if exists(file):
             print(f'{file} already downloaded')
         else:
+            print(f'downloading {file}')
             r = requests.get(clip_url)
             with open(file, 'wb') as f:
                 f.write(r.content)
@@ -89,11 +90,11 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    # project_dir = Path(__file__).resolve().parents[2]
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    # # find .env automagically by walking up directories until it's found, then
+    # # load up the .env entries as environment variables
+    # load_dotenv(find_dotenv())
 
     main()
 
